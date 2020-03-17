8fbd9422828f72e6ca9e372316f5f65d858ba99d
Alec N <alecng94@gmail.com>
2020-03-17 08:43:57 -0700

    render compare prop for dropzone for dragenter effect

--- src/edit-mode/components/universal/dropzone.js
+++ src/edit-mode/components/universal/dropzone.js
@@ -3,15 +3,14 @@ import styled from "styled-components";
 import FocusableContainer from './focusable-container';
 
 const DropZoneDiv = styled.div`
-  z-index: 9999;
   height: 15px;
   background-color: ${props => props.dragEnter
-    ? "rgba(0, 0, 0, 0.1)"
-    : "rgba(0, 0, 0, 0.03)"
+    ? "rgba(0, 0, 0, 0.1);"
+    : "rgba(0, 0, 0, 0.03);"
   };
   border: ${props => props.dragEnter
-    ? "1px dashed rgba(0, 0, 0, 0.75)"
-    : "1px dashed rgba(0, 0, 0, 0.15)"
+    ? "1px dashed rgba(0, 0, 0, 0.75);"
+    : "1px dashed rgba(0, 0, 0, 0.15);"
   };
   transition: border 0.25s, background-color 0.25s;  
   margin: -1px;
@@ -50,6 +49,7 @@ export default function DropZone({
       inPreviewMode={inPreviewMode}
       isFocused={isFocused}
       dataset={dataset}
+      renderCompareProp={dragEnter}
     >
       <DropZoneDiv
         data-uuid={uuid}
