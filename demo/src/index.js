221af7a2a0d9adf73fbbad78290234b36dfde6ed
Alec N <alecng94@gmail.com>
2020-03-28 10:10:01 -0700

    add font styles to demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
 
 import { BrandywineEditor } from '../../src/edit-mode/index';
 import { BrandywineReader } from '../../src/read-mode/index';
